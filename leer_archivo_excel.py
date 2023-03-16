5ead284dd5aadbd04af597e297b7a7657c4ed49b
unknown <Usuario@DESKTOP-TBJK1QB.ssp.cdmx.gob.mx>
2023-03-16 10:13:39 -0600

    Leer archivos de excel usando la funcion tabulate

---
+++ leer_archivo_excel.py
@@ -0,0 +1,23 @@
+# ller_archivo_excel.py
+# pip install openpyxl, tabulate
+
+import openpyxl
+from tabulate import tabulate
+
+excel_dataframe = openpyxl.load_workbook("plantilla.xlsx")
+dataframe = excel_dataframe.active
+
+#print(dataframe)
+
+data = []
+
+for row in range(1, dataframe.max_row):
+    #print(row)
+    _row = [row,]
+    for col in dataframe.iter_cols(1,dataframe.max_column-1):
+        _row.append(col[row].value)
+
+    data.append(_row)
+
+#print(data)
+print(tabulate(data))
